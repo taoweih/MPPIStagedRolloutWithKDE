4d5ec46e245bc019108b1f8bcba7ba15112c4e04
Taowei Huang <taoweihuang914@gmail.com>
2025-09-01 12:47:34 -0500

    modify drone task

--- hydrax/examples/u_drone.py
+++ hydrax/examples/u_drone.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     # Set up the controller
     ctrl = MPPI(
         task,
-        num_samples=512,
-        noise_level=2.0,
+        num_samples=128,
+        noise_level=1.0,
         temperature=0.001,
         num_randomizations=1,
-        plan_horizon=0.5,
+        plan_horizon=1.0,
         spline_type="zero",
         num_knots=16,
     )
@@ -30,14 +30,14 @@ if __name__ == "__main__":
     mj_model.opt.timestep = 0.01
 
     mj_data = mujoco.MjData(mj_model)
-    # mj_data.qpos[:] = mj_model.keyframe("hover").qpos
-    # mj_data.ctrl[:] = mj_model.keyframe("hover").ctrl
+    mj_data.qpos[:] = mj_model.keyframe("hover").qpos
+    mj_data.ctrl[:] = mj_model.keyframe("hover").ctrl
 
     run_interactive(
             ctrl,
             mj_model,
             mj_data,
             frequency=25,
-            show_traces=False,
+            show_traces=True,
             record_video=False,
         )
--- hydrax/hydrax/tasks/u_drone.py
+++ hydrax/hydrax/tasks/u_drone.py
@@ -18,7 +18,7 @@ class UDrone(Task):
         mj_model.opt.timestep = 0.01
         super().__init__(
             mj_model,
-            trace_sites= None #["imu_in_torso", "left_foot", "right_foot"],
+            trace_sites= ["tracking_site"] #["imu_in_torso", "left_foot", "right_foot"],
         )
 
         self.end_effector_pos_id = mujoco.mj_name2id(
@@ -28,16 +28,47 @@ class UDrone(Task):
             self.mj_model, mujoco.mjtObj.mjOBJ_BODY, "goal"
         )
 
+        self.body_id = mujoco.mj_name2id(
+            self.mj_model, mujoco.mjtObj.mjOBJ_BODY, "x2"
+        )
+
     def running_cost(self, state: mjx.Data, control: jax.Array) -> jax.Array:
         end_effector_pos = state.site_xpos[self.end_effector_pos_id]
         goal_pos = state.xpos[self.goal_pos_id]
+        pos_cost = jnp.sum(jnp.square(end_effector_pos - goal_pos),axis=0)
 
-        cost = 10*jnp.sum(jnp.square(end_effector_pos - goal_pos),axis=0)
+        v_world = state.qvel[:3]
+        w_world = state.qvel[3:6]
+        vel_cost     = jnp.sum(v_world ** 2)
+        angvel_cost  = jnp.sum(w_world ** 2)
+
+        R = state.xmat[self.body_id].reshape(3, 3)
+        tilt_err = 1.0 - R[2, 2]
+        tilt_cost = tilt_err ** 2
+
+        u = control
+        u_hover = jnp.full_like(u, self.mj_model.keyframe("hover").ctrl)
+        effort_cost = jnp.sum((u - u_hover) ** 2)
+
+        z = state.xpos[self.body_id, 2]
+        alt_cost = jnp.maximum(0.0, 0.5 - z) ** 2
+
+        cost = 50*pos_cost + 10 * tilt_cost + 0.1*effort_cost + 10*alt_cost + 0.1*vel_cost + 0.01 * angvel_cost
         return cost
 
     def terminal_cost(self, state: mjx.Data) -> jax.Array:
         """The terminal cost ϕ(x_T)."""
-        return self.running_cost(state, jnp.zeros(self.model.nu))
+        end_effector_pos = state.site_xpos[self.end_effector_pos_id]
+        goal_pos = state.xpos[self.goal_pos_id]
+        pos_cost = jnp.sum(jnp.square(end_effector_pos - goal_pos),axis=0) ** 4
+        # return self.running_cost(state, jnp.zeros(self.model.nu))
+        return pos_cost
+    
+    def success_function(self, state, control):
+        end_effector_pos = state.site_xpos[self.end_effector_pos_id]
+        goal_pos = state.xpos[self.goal_pos_id]
+        pos_cost = jnp.sum(jnp.square(end_effector_pos - goal_pos),axis=0) 
+        return pos_cost
 
     def domain_randomize_model(self, rng: jax.Array) -> Dict[str, jax.Array]:
         """Randomize the friction parameters."""
